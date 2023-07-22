2433f9abb166ba5a7823076c5e20984552c0ca08
Julie <juliebarreto897@gmail.com>
2023-07-22 15:14:18 -0500

    added Create a review by plantId

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ from .api.plant_routes import plant_routes
 from .api.cart_routes import cart_routes
 from .api.order_routes import order_routes
 from .api.payment_routes import payment_routes
+from .api.review_routes import review_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -35,7 +36,8 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(plant_routes, url_prefix='/api/plants')
 app.register_blueprint(cart_routes, url_prefix='/api/cart')
 app.register_blueprint(order_routes, url_prefix='/api/orders')
-app.register_blueprint(payment_routes, url_prefix='/api/payments' )
+app.register_blueprint(payment_routes, url_prefix='/api/payments')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 db.init_app(app)
 Migrate(app, db)
 
