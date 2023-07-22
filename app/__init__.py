9fb0172a0a4df93d38af588401b456eb8849e51e
Julie <juliebarreto897@gmail.com>
2023-07-22 16:43:47 -0500

    added GET all current user's favorites

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ from .api.cart_routes import cart_routes
 from .api.order_routes import order_routes
 from .api.payment_routes import payment_routes
 from .api.review_routes import review_routes
+from .api.favorite_routes import favorite_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -38,6 +39,7 @@ app.register_blueprint(cart_routes, url_prefix='/api/cart')
 app.register_blueprint(order_routes, url_prefix='/api/orders')
 app.register_blueprint(payment_routes, url_prefix='/api/payments')
 app.register_blueprint(review_routes, url_prefix='/api/reviews')
+app.register_blueprint(favorite_routes, url_prefix='/api/favorites')
 db.init_app(app)
 Migrate(app, db)
 
