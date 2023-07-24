3dac05c38ae5359e4e0621d5b01efa72d352cdc0
Julie <juliebarreto897@gmail.com>
2023-07-24 18:20:14 -0500

    added delete and get review to the store

--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -1,8 +1,20 @@
 const CREATE_REVIEW = 'review/CREATE_REVIEW'
+const GET_REVIEWS = "review/GET_REVIEWS"
+const DELETE_REVIEW = "review/DELETE_REVIEW"
 
 const createReview = (review) => ({
     type: CREATE_REVIEW,
     payload: review
+})
+
+const getReviews = (reviews) => ({
+    type:GET_REVIEWS,
+    payload: reviews
+})
+
+const deleteReview = (reviewId) => ({
+    type:DELETE_REVIEW,
+    payload: reviewId
 })
 
 export const createReviewforPlant = (plantId, review) => async(dispatch) => {
@@ -23,12 +35,33 @@ export const createReviewforPlant = (plantId, review) => async(dispatch) => {
     }
 }
 
+export const getAllPlantReviews = (plantId) => async(dispatch) => {
+    const response = await fetch(`/api/plants/${plantId}/reviews`);
+    if(response.ok){
+        const data = await response.json();
+        dispatch(getReviews(data));
+    }
+}
+
+export const deleteReviewById = (reviewId) => async(dispatch) => {
+    const response = await fetch(`/api/reviews/${reviewId}`, {
+        method: 'DELETE'
+    })
+    if(response.ok){
+        dispatch(deleteReview(reviewId))
+    }
+}
+
 const initialState = [];
 
 const reviewReducer = (state = initialState, action) => {
     switch(action.type){
         case CREATE_REVIEW:
             return [...state, action.payload]
+        case GET_REVIEWS:
+            return action.payload
+        case DELETE_REVIEW:
+            return state.filter(review => review.id !== action.payload)
         default:
             return state
     }
