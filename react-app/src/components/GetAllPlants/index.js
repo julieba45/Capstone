93ce7e54e2eb7b790855aa00abdea673dad0b12d
Julie <juliebarreto897@gmail.com>
2023-07-26 13:41:08 -0500

    adding a plant to favorites works, but need to remove the hardcoded default garden

--- react-app/src/components/GetAllPlants/index.js
+++ react-app/src/components/GetAllPlants/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPlants } from '../../store/plant';
 import { useHistory } from 'react-router-dom';
+import { addFavoritePlant } from '../../store/favorite';
 
 const GetAllPlants = () => {
     const dispatch = useDispatch()
@@ -17,6 +18,10 @@ const GetAllPlants = () => {
         history.push(`/plants/${plantId}`)
     }
 
+    const AddingFavoritePlant = (plantId, gardenName, position) => {
+        dispatch(addFavoritePlant(plantId, gardenName, position))
+    }
+
     return (
         <div>
             <h1>Plant List</h1>
@@ -26,6 +31,7 @@ const GetAllPlants = () => {
                         <h2>{plant.name}</h2>
                         <p>{plant.description}</p>
                         <button onClick={() => handleClick(plant.id)}>See Details</button>
+                        <button onClick={() => AddingFavoritePlant(plant.id, 'Default Garden', 1)}>Add to Favorites</button>
                     </div>
                 ))
             }
