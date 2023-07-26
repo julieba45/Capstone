5adde4db665545d32cbcd830f40df77edee88380
Julie <juliebarreto897@gmail.com>
2023-07-25 20:32:49 -0500

    added simple update to test functionality

--- migrations/versions/20230725_202344_.py
+++ migrations/versions/20230725_202344_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 123dc4334d0b
+Revision ID: 6f9505a3233f
 Revises: 
-Create Date: 2023-07-25 13:41:45.615895
+Create Date: 2023-07-25 20:23:44.147947
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '123dc4334d0b'
+revision = '6f9505a3233f'
 down_revision = None
 branch_labels = None
 depends_on = None
--- react-app/src/components/MyFavoritesPage/index.js
+++ react-app/src/components/MyFavoritesPage/index.js
@@ -1,50 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFavorites } from "../../store/favorite";
 import { NavLink } from 'react-router-dom';
-import DraggablePlant from "../DraggablePlant";
-import DroppableGarden from "../DroppableGarden";
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { updateFavoritePlant } from "../../store/favorite";
 
 const MyFavoritesPage = () => {
     const dispatch = useDispatch()
     const favorites = useSelector(state => state.favorites)
+    const [reload, setReload] = useState(false);
 
     useEffect(() => {
         console.log('HEYYYY------', favorites)
         dispatch(fetchFavorites())
-    }, [dispatch])
+    }, [dispatch, reload])
+
+    const handleUpdateClick = async (favoriteId) => {
+        await dispatch(updateFavoritePlant(favoriteId, "New Garden Name"));
+        setReload(!reload);
+
+    }
 
     return (
-        <DndProvider backend={HTML5Backend}>
-          <h1>Favorites</h1>
-          {favorites.map((favorite) => (
-            <div key={favorite.id}>
-              <DraggablePlant plant={favorite.plant} favorite={favorite} />
-              <DroppableGarden gardenName={favorite.gardenName} dispatch={dispatch} />
-            </div>
-          ))}
-        </DndProvider>
-      );
-};
-
-
-// return (
-//     <div>
-//         <h1>Favorites</h1>
-//         {favorites.map((favorite) => (
-//             <div key={favorite.id}>
-//                 <h2>
-//                     Garden Name: <NavLink to={`/garden/${favorite.gardenName}`}>{favorite.gardenName}</NavLink>
-//                 </h2>
-//                 <p>Plant Name:{favorite.plant.name}</p>
-//                 {/* <img src={favorite.plant.primary_image} alt="Plant" /> */}
-//                 {/* <p>Created at: {new Date(favorite.createdAt).toLocaleString()}</p>
-//                 <p>Updated at: {new Date(favorite.updatedAt).toLocaleString()}</p> */}
-//             </div>
-//         ))}
-//     </div>
-// )
+        <div>
+            <h1>Favorites</h1>
+            {favorites.map((favorite) => (
+                <div key={favorite.id}>
+                    <h2>
+                        Garden Name: <NavLink to={`/garden/${favorite.gardenName}`}>{favorite.gardenName}</NavLink>
+                    </h2>
+                    <p>Plant Name:{favorite.plant.name}</p>
+                    {/* <img src={favorite.plant.primary_image} alt="Plant" /> */}
+                    {/* <p>Created at: {new Date(favorite.createdAt).toLocaleString()}</p>
+                    <p>Updated at: {new Date(favorite.updatedAt).toLocaleString()}</p> */}
+                     <button onClick={() => handleUpdateClick(favorite.id)}>Move to New Garden</button>
+                </div>
+            ))}
+        </div>
+    )
+}
 
 export default MyFavoritesPage
