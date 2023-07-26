ca427c8616c7d786e0b2580fa1235e066a604395
Julie <juliebarreto897@gmail.com>
2023-07-26 16:53:20 -0500

    edited text input area in garden modal

--- react-app/src/components/GardenSelectionModal/index.js
+++ react-app/src/components/GardenSelectionModal/index.js
@@ -1,6 +1,13 @@
-
+import React, {useState} from "react";
 
 const GardenSelectionModal = ({onGardenSelect, gardenNames}) => {
+    const [newGardenName, setNewGardenName] = useState('');
+
+    const handleNewGardenSubmit = (e) => {
+        e.preventDefault()
+        onGardenSelect(newGardenName)
+        setNewGardenName('');
+    }
     return (
         <div>
             <h2>Select a Garden</h2>
@@ -9,6 +16,16 @@ const GardenSelectionModal = ({onGardenSelect, gardenNames}) => {
                     {gardenName}
                 </button>
             )}
+            <form onSubmit={handleNewGardenSubmit}>
+                <input
+                    type="text"
+                    placeholder="New Garden Name"
+                    value={newGardenName}
+                    onChange={(e) => setNewGardenName(e.target.value)}
+                />
+                <button type="submit">Create New Garden</button>
+
+            </form>
         </div>
     )
 
