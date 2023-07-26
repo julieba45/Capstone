501decd893414203307113fe48599b0d6b2ff16f
Julie <juliebarreto897@gmail.com>
2023-07-25 22:31:56 -0500

    almost there manual draggable

--- react-app/src/components/MyFavoritesPage/index.js
+++ react-app/src/components/MyFavoritesPage/index.js
@@ -9,30 +9,49 @@ const MyFavoritesPage = () => {
     const favorites = useSelector(state => state.favorites)
     const [reload, setReload] = useState(false);
 
+    const handleDragStart = (event, plantId) => {
+        event.dataTransfer.setData("plantId", plantId);
+    }
+
+    const handleDrop = async (event, gardenName) => {
+        const plantId = event.dataTransfer.getData("plantId");
+        await dispatch(updateFavoritePlant(plantId, gardenName));
+        setReload(!reload);
+    }
+
     useEffect(() => {
-        console.log('HEYYYY------', favorites)
+        // console.log('HEYYYY------', favorites)
         dispatch(fetchFavorites())
     }, [dispatch, reload])
 
-    const handleUpdateClick = async (favoriteId) => {
-        await dispatch(updateFavoritePlant(favoriteId, "New Garden Name"));
-        setReload(!reload);
+    // const handleUpdateClick = async (favoriteId) => {
+    //     await dispatch(updateFavoritePlant(favoriteId, "New Garden Name"));
+    //     setReload(!reload);
 
-    }
+    // }
 
     return (
         <div>
             <h1>Favorites</h1>
             {favorites.map((favorite) => (
-                <div key={favorite.id}>
+                <div
+                    key={favorite.id}>
                     <h2>
                         Garden Name: <NavLink to={`/garden/${favorite.gardenName}`}>{favorite.gardenName}</NavLink>
                     </h2>
-                    <p>Plant Name:{favorite.plant.name}</p>
+                    <div
+                        onDrop={(event) => handleDrop(event, favorite.gardenName)}
+                        onDragOver={(event) => event.preventDefault()}
+                    >
+                    <p
+                        draggable
+                        onDragStart={(event) => handleDragStart(event, favorite.plant.id)}
+                    >Plant Name:{favorite.plant.name}</p>
+                    </div>
                     {/* <img src={favorite.plant.primary_image} alt="Plant" /> */}
                     {/* <p>Created at: {new Date(favorite.createdAt).toLocaleString()}</p>
                     <p>Updated at: {new Date(favorite.updatedAt).toLocaleString()}</p> */}
-                     <button onClick={() => handleUpdateClick(favorite.id)}>Move to New Garden</button>
+                     {/* <button onClick={() => handleUpdateClick(favorite.id)}>Move to New Garden</button> */}
                 </div>
             ))}
         </div>
