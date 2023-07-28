80c67cd3916aeb7ba9b317fdddfca601cf37f23d
Julie <juliebarreto897@gmail.com>
2023-07-27 21:03:32 -0500

    added googlemaps api for a dynamic map

--- react-app/src/components/ConfirmationPage/index.js
+++ react-app/src/components/ConfirmationPage/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { getOrder } from "../../store/cart";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 
 const ConfirmationPage = () => {
@@ -11,20 +12,48 @@ const ConfirmationPage = () => {
     const order = useSelector(state => state.cart.order)
 
     const [loading, setLoading] = useState(true);
+    const [mapData, setMapData] = useState(null);
+
 
     useEffect(() => {
         console.log("---------ORDER AND ORDERID",order, orderId)
-        dispatch(getOrder(orderId))
-        .then(() => setLoading(false))
-        .catch(err => {
-            console.error(err);
+        if (!order) {
+            dispatch(getOrder(orderId))
+            .catch(err => {
+                console.error(err);
+                setLoading(false);
+            });
+        } else if (order.payment) {
             setLoading(false);
-        });
-    }, [dispatch, orderId]);
+            fetchMapData(order.payment.location);
+            console.log('hi')
+        }
+    }, [dispatch, orderId, order]);
 
-    if (loading) {
+    const fetchMapData = async (location) => {
+        try {
+            const response = await fetch(`/api/auth/map/${location}`);
+            const data = await response.json();
+            setMapData(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
+    if (loading || !mapData) {
         return <div>Loading...</div>
     }
+
+
+    const mapStyles = {
+        height: "50vh",
+        width: "100%"};
+
+    const defaultCenter = {
+        lat: mapData.results[0].geometry.location.lat, lng: mapData.results[0].geometry.location.lng
+    }
+
 
     return (
         <div>
@@ -34,8 +63,17 @@ const ConfirmationPage = () => {
             <p>Order for: {order.userFirstName}</p>
             <p>Total Amount: {order.payment.paymentAmount}</p>
             <p>Order Status: {order.status}</p>
-
-
+            <p>Shipped Location: {order.payment.location}</p>
+            {mapData && mapData.results &&
+                <LoadScript googleMapsApiKey='AIzaSyCyV7tOD88GazFpAs6JWIUEzwibOnQE3kU'>
+                    <GoogleMap
+                        mapContainerStyle={mapStyles}
+                        zoom={13}
+                        center={defaultCenter}>
+                        <Marker key={defaultCenter.lat} position={defaultCenter}/>
+                    </GoogleMap>
+                </LoadScript>
+            }
             <NavLink to='/'>Return to Home</NavLink>
         </div>
     )
