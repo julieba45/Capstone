8205059f223be9c7f80a6901626d50484dd08a89
Julie <juliebarreto897@gmail.com>
2023-07-29 09:32:32 -0500

    Signup validations

--- react-app/src/components/CarePage/index.js
+++ react-app/src/components/CarePage/index.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { fetchOrders } from "../../store/order";
 
 const CarePage = () => {
     const location = useSelector(state => state.session.user.location)
-
     const [weatherData, setWeatherData] = useState(null);
-    const [lat, setLat] = useState(null)
-    const [lon, setLon] = useState(null)
     const [loading, setLoading] = useState(true);
+    const orders = useSelector(state => state.orders.orders);
+    const dispatch = useDispatch();
 
-    const city = location.split(', ')[location.split(', ').length - 3];
-
+    useEffect(() => {
+        dispatch(fetchOrders())
+    }, [dispatch]);
 
     useEffect(() => {
         const fetchWeatherData = async () => {
@@ -19,7 +19,7 @@ const CarePage = () => {
                 try{
                 const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=${process.env.REACT_APP_API_KEY}`);
                 const data = await response.json();
-                console.log('-----------LOCATION DATA', data)
+                // console.log('-----------LOCATION DATA', data)
                 const { lat, lng } = data.results[0].geometry.location;
 
                 const weatherResponse = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lng}/today?key=${process.env.REACT_APP_API_WEATHER}`)
@@ -38,9 +38,9 @@ const CarePage = () => {
         fetchWeatherData();
     }, [location])
 
-    // if (loading) {
-    //     return <div>Loading...</div>
-    // }
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
@@ -49,10 +49,23 @@ const CarePage = () => {
                 <>
                     <p>Temperature: {Math.round(weatherData.days[0].temp)}°F</p>
                     <p>Weather: {weatherData.days[0].conditions}</p>
-                    {/* <p>Description: {weatherData.weather[0].description}</p> */}
                     <p>Precipitation: {Math.round(weatherData.days[0].precip*100)}%</p>
                 </>
             )}
+            {orders.map(order => (
+                 order.status === "Completed" && order.orderPlants.map(orderPlant => {
+                    const wateringAmount = orderPlant.plant.wateringFrequency * (1 - (weatherData.days[0].precip));
+                    return (
+                        <div key={orderPlant.id}>
+                            <p>Plant Name: {orderPlant.plant.name}</p>
+                            <p>Plant Quantity: {orderPlant.quantity}</p>
+                            <p>Plant watering frequency: {orderPlant.plant.wateringFrequency} per day</p>
+                            <p>Adjusted watering amount based on precipitation: {wateringAmount} per day</p>
+                            <p>Care Instructions: {orderPlant.plant.careInstructions}</p>
+                        </div>
+                    )
+                })
+            ))}
 
         </div>
     )
