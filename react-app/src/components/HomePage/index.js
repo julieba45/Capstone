46f44c4f42dea136afac1c2e811327ad1c0db311
Julie <juliebarreto897@gmail.com>
2023-07-29 22:58:36 -0500

    setup backend ai chat correctly, having frontend issues with fetch

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,13 +1,80 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import { getPlants } from '../../store/plant';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Spline from '@splinetool/react-spline';
 
 
 const HomePage = () => {
+    const [history, setHistory] = useState(JSON.parse(sessionStorage.getItem('chatHistory')) || []);
+    const [input, setInput] = useState('');
+    const dispatch = useDispatch()
+    const plants = useSelector(state => state.plants.allPlants);
+
+    useEffect(() => {
+        dispatch(getPlants());
+    }, [dispatch])
+
+    useEffect(() => {
+        sessionStorage.setItem('chatHistory', JSON.stringify(history));
+    }, [history])
+
+    const handleSubmit = async() => {
+        const newMessage = {
+            role:'user',
+            content: input
+        };
+        setHistory([...history, newMessage])
+
+    try{
+        const plantsList = plants.map(plant => `'${plant.name}'`).join(", ");
+        const systemMessageContent = `You are a helpful assistant. Current plants in inventory: ${plantsList}.`;
+        const response = await fetch('/api/chat', {
+            method: 'POST',
+            // mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                messages: [
+                    {
+                        role: "system",
+                        content: systemMessageContent
+                    },
+                    newMessage
+                ]
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`An error has occured. status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const assistantMessage = {
+            role: 'assistant',
+            content: data.response
+        };
+        setHistory(prevHistory => [...prevHistory, assistantMessage]);
+        setInput('');
+
+    }catch(error){
+        console.error('Fetch error:', error);
+    }
+}
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+    };
+
     return (
         <div>
             <h1>HomePage</h1>
-            {/* <Spline scene="https://prod.spline.design/JUsF9Xae24truzF4/scene.splinecode" /> */}
+            <div>
+                {history.map((message, index) => (
+                    <p key={index}><strong>{message.role}:</strong> {message.content}</p>
+                ))}
+            </div>
+            <input value={input} onChange={handleInputChange} />
+            <button onClick={handleSubmit}>Send</button>
 
         </div>
     )
