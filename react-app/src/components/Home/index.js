acf9d6baf69d2509f1ccdc5c7c4085f8ab931a3c
Julie <juliebarreto897@gmail.com>
2023-07-30 23:28:07 -0500

    added navbar styles

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ChatBot from "../ChatBot";
+import "./Home.css";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="Home">Home</h1>
             <ChatBot/>
         </div>
     )
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,28 +4,40 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import CartCount from '../CartCount';
+import BloomLogo from "./images/Bloom (3).png"
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
+		<nav className="navbar">
+			<ul>
 				<li>
-					<ProfileButton user={sessionUser} />
+					{isLoaded && (
+					<div>
+						<NavLink className="home-store" to="/plants">Store</NavLink>
+					</div>
+
+					)}
+				 </li>
+				 <li>
+				<NavLink exact to="/">
+					<img className='logoimg' src={BloomLogo} alt='logo' />
+				</NavLink>
 				</li>
-			)}
-			{isLoaded && (
-				<NavLink to="/plants">Store</NavLink>
-			)}
-			{isLoaded && <CartCount />}
-			{isLoaded && sessionUser &&(
-				<NavLink to='/care'>Care</NavLink >
-			)}
-		</ul>
+				<li className='care-cart-profile'>
+				{isLoaded && sessionUser &&(
+					<NavLink to='/care'>Care</NavLink >
+				)}
+				{isLoaded && <CartCount />}
+				{isLoaded && (
+					<li>
+						<ProfileButton user={sessionUser} />
+					</li>
+				)}
+				</li>
+			</ul>
+		</nav>
 	);
 }
 
