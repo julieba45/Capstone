df57138ab6a7517ab5614a47d2d6c388b6975f25
Julie <juliebarreto897@gmail.com>
2023-08-03 17:41:23 -0500

    changes to LoginForm and Modals

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ChatBot from "../ChatBot";
 import "./Home.css";
 import Spline from '@splinetool/react-spline';
-
+import homeplants from "../../public/homeplants-2.mp4"
 const Home = () => {
     return (
-        <div>
-            <h1 className="Home">Home</h1>
+        <div className="home-container">
+            <div className="home-content">
+                <h1 className="Home">Home</h1>
+            </div>
             {/* <Spline scene="https://prod.spline.design/dO2sGncbnQ3-xKEL/scene.splinecode" /> */}
+            {/* <video className="video-container" autoPlay muted>
+                <source src={homeplants} type="video/mp4" />
+            </video> */}
             <ChatBot/>
         </div>
     )
