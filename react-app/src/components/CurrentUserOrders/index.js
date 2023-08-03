c5429a5b670464e521384b341798a36d5f0342b6
Julie <juliebarreto897@gmail.com>
2023-08-03 17:41:23 -0500

    changes to LoginForm and Modals

--- react-app/src/components/CurrentUserOrders/index.js
+++ react-app/src/components/CurrentUserOrders/index.js
@@ -26,21 +26,24 @@ const CurrentUserOrders = () => {
                 <React.Fragment key={order.id}>
                 <p className="order-placed"> Order placed: {new Date(order.createdAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
                 <div className="order-grid" key={order.id}>
-                    <h3>Order No. {order.id}</h3>
-                    {/* <p>{order.userFirstName}</p> */}
+                    <div className="order-top-half">
+                        <h3>Order No. {order.id}</h3>
+                        {/* <p>{order.userFirstName}</p> */}
 
-                    <p className="order-status">Status: {order.status}</p>
-                    {/* <p>'HEYY'{order.payment.id}</p> */}
-                    {order.payment && <p>Payment: $ {order.payment.paymentAmount}</p>}
-
-                    <h4 className="products-bought-title">Products:</h4>
-                    {order.orderPlants.map(orderPlant => (
-                         <div className="order-plants-info"key={orderPlant.id}>
-                         <p>Plant Name: {orderPlant.plant.name}</p>
-                         <p>Plant Quantity: {orderPlant.quantity}</p>
-                         <p>Plant watering frequency: {orderPlant.plant.wateringFrequency}</p>
-                     </div>
-                    ))}
+                        <p className="order-status">Status: {order.status}</p>
+                        {/* <p>'HEYY'{order.payment.id}</p> */}
+                        {order.payment && <p>Payment: $ {order.payment.paymentAmount}</p>}
+                    </div>
+                    <div className="order-bottom-half">
+                        <h4 className="products-bought-title">Products:</h4>
+                        {order.orderPlants.map(orderPlant => (
+                            <div className="order-plants-info"key={orderPlant.id}>
+                            <p>Plant Name: {orderPlant.plant.name}</p>
+                            <p>Plant Quantity: {orderPlant.quantity}</p>
+                            <p>Plant watering frequency: {orderPlant.plant.wateringFrequency}</p>
+                            </div>
+                        ))}
+                    </div>
                      {order.status !== 'Cancelled' && (
                         <button  className="cancel-btn-orders" onClick={() => openCancelModal(order.payment.id)}>
                             Cancel Payment
