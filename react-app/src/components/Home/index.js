49dbe172a7ff9dec05f6ffa4dc5ee3aaba08e9c2
Julie <juliebarreto897@gmail.com>
2023-08-02 23:22:04 -0500

    moved signupmodal logic to signup page instead

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ChatBot from "../ChatBot";
 import "./Home.css";
+import Spline from '@splinetool/react-spline';
 
 const Home = () => {
     return (
         <div>
             <h1 className="Home">Home</h1>
+            {/* <Spline scene="https://prod.spline.design/dO2sGncbnQ3-xKEL/scene.splinecode" /> */}
             <ChatBot/>
         </div>
     )
