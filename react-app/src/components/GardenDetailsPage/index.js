46b68ad73d1daf73e4c9f38dda2d66108ef8265f
Julie <juliebarreto897@gmail.com>
2023-08-02 19:30:57 -0500

    fixed minor loading bug

--- react-app/src/components/GardenDetailsPage/index.js
+++ react-app/src/components/GardenDetailsPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { deleteFavoritePlant, fetchGardenFavorites } from '../../store/favorite';
@@ -9,9 +9,13 @@ const GardenDetailsPage = () => {
     const dispatch = useDispatch();
     const { gardenName } = useParams();
     const favorites = useSelector(state => state.favorites);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        dispatch(fetchGardenFavorites(gardenName));
+        setLoading(true);
+        dispatch(fetchGardenFavorites(gardenName)).then(() => {
+            setLoading(false);
+        });
     }, [dispatch, gardenName]);
 
     const handleDelete = (favoriteId) => {
@@ -20,31 +24,37 @@ const GardenDetailsPage = () => {
 
     return (
         <div>
-        <div className='garden-title-container'>
-            <h1 className='garden-title'>{gardenName}</h1>
-            <p className='collection-number'>248</p>
-        </div>
-        <div className='grid-favorite-details'>
-        {favorites.map((favorite) => (
-                <div key={favorite.id} className='grid-favorite-item'>
-                    <div className='favorite-image-container'>
-                    <img src={favorite.plantPrimaryImage.primary_image} alt={favorite.plant.name}></img>
-                    </div>
-                    <p>Plant Name: {favorite.plant.name}</p>
-                    {/* <img src={plantPrimaryImage.primary_image} alt={favorite.plant.name} /> */}
-                    <p>Species: {favorite.plant.species}</p>
-                    <p>Size: {favorite.plant.size}</p>
-                    {/* <p>Description: {favorite.plant.description}</p> */}
-                    {/* <p>Care Instructions: {favorite.plant.careInstructions}</p> */}
-                    <p>Price: $ {favorite.plant.price}</p>
-                    <p>Is In Bloom: {favorite.plant.isInBloom ? 'Yes' : 'No'}</p>
-                    <p>Watering Frequency: {favorite.plant.wateringFrequency}</p>
-                    {/* <p>Created at: {new Date(favorite.createdAt).toLocaleString()}</p>
-                    <p>Updated at: {new Date(favorite.updatedAt).toLocaleString()}</p> */}
-                    <button onClick={() => handleDelete(favorite.id)}>Remove</button>
+            {loading ? (
+            <div>Loading...</div>
+            ) : (
+            <div>
+                <div className='garden-title-container'>
+                    <h1 className='garden-title'>{gardenName}</h1>
+                    <p className='collection-number'>248</p>
                 </div>
-            ))}
-        </div>
+                <div className='grid-favorite-details'>
+                {favorites.map((favorite) => (
+                        <div key={favorite.id} className='grid-favorite-item'>
+                            <div className='favorite-image-container'>
+                            <img src={favorite.plantPrimaryImage.primary_image} alt={favorite.plant.name}></img>
+                            </div>
+                            <p>Plant Name: {favorite.plant.name}</p>
+                            {/* <img src={plantPrimaryImage.primary_image} alt={favorite.plant.name} /> */}
+                            <p>Species: {favorite.plant.species}</p>
+                            <p>Size: {favorite.plant.size}</p>
+                            {/* <p>Description: {favorite.plant.description}</p> */}
+                            {/* <p>Care Instructions: {favorite.plant.careInstructions}</p> */}
+                            <p>Price: $ {favorite.plant.price}</p>
+                            <p>Is In Bloom: {favorite.plant.isInBloom ? 'Yes' : 'No'}</p>
+                            <p>Watering Frequency: {favorite.plant.wateringFrequency}</p>
+                            {/* <p>Created at: {new Date(favorite.createdAt).toLocaleString()}</p>
+                            <p>Updated at: {new Date(favorite.updatedAt).toLocaleString()}</p> */}
+                            <button onClick={() => handleDelete(favorite.id)}>Remove</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            )}
         </div>
     )
 }
