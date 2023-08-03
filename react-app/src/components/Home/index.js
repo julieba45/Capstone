39bce1785eddeb5bca06a95f0ee76b3dab940538
Julie <juliebarreto897@gmail.com>
2023-08-03 17:54:06 -0500

    added simple home video

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -6,13 +6,13 @@ import homeplants from "../../public/homeplants-2.mp4"
 const Home = () => {
     return (
         <div className="home-container">
-            <div className="home-content">
+            {/* <div className="home-content">
                 <h1 className="Home">Home</h1>
-            </div>
+            </div> */}
             {/* <Spline scene="https://prod.spline.design/dO2sGncbnQ3-xKEL/scene.splinecode" /> */}
-            {/* <video className="video-container" autoPlay muted>
+            <video className="video-container" autoPlay muted>
                 <source src={homeplants} type="video/mp4" />
-            </video> */}
+            </video>
             <ChatBot/>
         </div>
     )
